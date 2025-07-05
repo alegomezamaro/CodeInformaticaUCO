3268743281cf0d4c152230543fa9741f19c8c58a
Alejandro Gómez Amaro <alexgoama@gmail.com>
2025-07-05 10:22:21 +0200

    .

--- PAS/practica1/ejercicios/ejercicio1.sh
+++ PAS/practica1/ejercicios/ejercicio1.sh
@@ -17,7 +17,8 @@ fi
 ficheros=$(find $directorio -type f) #Busca todos los ficheros (-type f) de "directorio"
 primerFichero=$(find $directorio -type f | head -n 1) #Coge el primer fichero de la lista
 lineas=$(cat $primerFichero | wc -l) #Cuenta las líneas del primer fichero
-total=0;
+total=0
+media=0
 
 for ((i=1; i<=$lineas; i=$i+1)); do #Recorremos todas las líneas
 
@@ -26,15 +27,14 @@ for ((i=1; i<=$lineas; i=$i+1)); do #Recorremos todas las líneas
     for fichero in $ficheros; do #Recorremos todos los ficheros
 
         lineaFichero=$(cat $fichero | head -n $i | tail -n 1) #Head lee las "i" primeras lineas y tail coge la ultima de ellas
-        let asistencia=$asistencia+$lineaFichero #Sumamos el número de asistencias en dicha sesion
+        asistencia=(($asistencia+$lineaFichero)) #Sumamos el número de asistencias en dicha sesion
     done
     
     echo "Asistieron $asistencia personas a la sesión $i"
-    let total=$total+$asistencia
+    total=(($total+$asistencia)) #Sumamos las asistencias totales
 
 done
 
-media=0
-let media=($total/$lineas)
+media=(($total/$lineas)) #Calculamos la media de asistencia  
 echo "La media de asistencia es: $media"
 exit 0 #Salir
